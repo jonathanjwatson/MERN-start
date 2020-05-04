542875479b0faa54e0d7ab1d28ef26e8fa45b3ce
Jonathan Watson <jonathanwatson@Jonathans-MacBook-Pro.local>
2020-05-04 14:24:21 -0400

    made changes to serve up react build folder from express

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const express = require("express");
+const path = require("path");
 
 const PORT = process.env.PORT || 3001;
 
@@ -11,8 +12,14 @@ app.use(express.json());
 app.get("/api/config", (req, res) => {
   res.json({
     success: true,
-    currentPort: PORT
+    currentPort: PORT,
   });
+});
+
+app.use(express.static("client/build"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "/client/build/index.html"));
 });
 
 app.listen(PORT, () => {
